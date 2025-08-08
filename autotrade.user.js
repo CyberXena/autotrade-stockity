bf6ea5ffa7891138626fa06ea446c001317dba77
CyberXena <72180020+CyberXena@users.noreply.github.com>
2025-08-08 08:06:20 +0700

    Update autotrade.user.js

--- autotrade.user.js
+++ autotrade.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AutoTrade Stockity Pro
 // @namespace    https://github.com/CyberXena
-// @version      1.7.7
+// @version      1.7.8
 // @description  Auto trading script untuk Stockity.id - Terhubung ke GitHub
 // @author       CyberXena
 // @match        https://stockity.id/trading
